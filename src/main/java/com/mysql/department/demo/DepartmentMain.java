81d11de6f1254ed1498bd60c8b347857dec8930b
hemeilong <hemeilong@tuzhanai.com>
2020-06-03 00:33:07 +0800

    更新地址为服务器内网地址@Reference(url="dubbo://172.21.0.7:20880/com.mysql.employee.demo.service.api.EmployeesService")

--- src/main/java/com/mysql/department/demo/DepartmentMain.java
+++ src/main/java/com/mysql/department/demo/DepartmentMain.java
@@ -15,7 +15,8 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan({"com.mysql.department.demo","com.mysql.department.demo.service.api"}) //因为这些文件用到了注解，需要加扫描，可扫很多注解
 @MapperScan("com.mysql.department.demo.dao") //扫描Mybatis，即dao文件下接口，该扫描专门用来扫Mybatis
 public class DepartmentMain {
-    @Reference(url="dubbo://192.168.0.101:20880/com.mysql.employee.demo.service.api.EmployeesService")
+//    @Reference(url="dubbo://192.168.0.101:20880/com.mysql.employee.demo.service.api.EmployeesService")
+    @Reference(url="dubbo://172.21.0.7:20880/com.mysql.employee.demo.service.api.EmployeesService")
 
 //    private EmployeesService employeesService;
 
