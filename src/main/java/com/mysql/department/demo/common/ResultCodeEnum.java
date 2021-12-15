f548a016709e816104151916fce460eacc63d8aa
hemeilong <373976324>
2021-12-16 01:20:18 +0800

    测试覆盖率

--- src/main/java/com/mysql/department/demo/common/ResultCodeEnum.java
+++ src/main/java/com/mysql/department/demo/common/ResultCodeEnum.java
@@ -5,8 +5,8 @@ public enum ResultCodeEnum {
     FAILED(69999, "department system error", "系统内部异常，请联系管理员"),
     OTHER_ERROR(68888, "other system error", "其他系统内部异常，请联系管理员"),
     DEPT_NUM_IS_NULL(67777, "deptNo can not be null", "部门id不能为空"),
-    REQ_PARAMS_ERROR(Integer.valueOf(60000), "req params error", "请求参数错误");
-
+    REQ_PARAMS_ERROR(Integer.valueOf(60000), "req params error", "请求参数错误"),
+    DEPT_NUM_IS_TEST_JACOCO(11111, "deptNo can is test_jacoco", "部门id为测试jacoco增量覆盖值");
 
     private Integer code;
     private String msg;
